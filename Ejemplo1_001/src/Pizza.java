24069a76bb33de169f92cff6087289f9fec5b3f4
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-17 08:44:03 -0400

    ejemplo

---
+++ Ejemplo1_001/src/Pizza.java
@@ -0,0 +1,23 @@
+
+public class Pizza {
+// 1.- atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+    
+// 2.- contructor
+
+// 3.- getter and setter
+
+// 4.- métodos customer o personalizado
+    public void preparar()
+    {
+        System.out.println("Se está preparando la pizza");
+    }
+    
+    public void cocinar()
+    {
+        System.out.println("Se esta cocinando la pizza");
+    }
+
+}
---
+++ Ejemplo1_001/src/Principal.java
@@ -0,0 +1,28 @@
+// comentario de una linea
+
+/*
+comentario para 
+más de una linea
+*/
+public class Principal {
+    
+    // el main es el punto de arranque de la app
+    public static void main(String[] args)
+    {
+        // escribir codigo java
+        System.out.println("Hola Mundo!!!");        
+        // sout + tab
+        
+        // crear un objeto pizza
+        Pizza pizza1 = new Pizza();
+        
+        Pizza pizza2;
+        pizza2 = new Pizza();
+        
+        
+        pizza1.cocinar();
+        pizza2.cocinar();
+        
+        
+    }    
+}
