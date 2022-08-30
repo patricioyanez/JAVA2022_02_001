4efbfa37b55bacaf3dbf4c1a0245db566f474fa7
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-30 16:39:55 -0400

    actividad

---
+++ ActividadGiftCard_001/src/GiftCard.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class GiftCard {
+    private long codigo;
+    private int clave;
+    private int monto;
+    private Date fecha;
+    private Trabajador trabajo;
+    public GiftCard() {
+        this.codigo = 0;
+        this.clave = 0;
+        this.monto = 0;
+        this.fecha = new Date(); // null;
+        this.trabajo = new Trabajador(); // null;
+    } 
+        
+    public GiftCard(long codigo, int clave, int monto, Date fecha, Trabajador trabajo) {
+        this.codigo = codigo;
+        this.clave = clave;
+        this.monto = monto;
+        this.fecha = fecha;
+        this.trabajo = trabajo;
+    }
+
+    public Trabajador getTrabajo() {
+        return trabajo;
+    }
+
+    public void setTrabajo(Trabajador trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
---
+++ ActividadGiftCard_001/src/Principal.java
@@ -0,0 +1,7 @@
+
+public class Principal {
+    // psvm + tab
+    public static void main(String[] args) {
+        
+    }
+}
