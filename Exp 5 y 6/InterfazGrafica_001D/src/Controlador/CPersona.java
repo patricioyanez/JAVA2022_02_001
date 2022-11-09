a652f95525dc776728192e53824edcf9d6a46154
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-09 12:57:39 -0300

    update

---
+++ Exp 5 y 6/InterfazGrafica_001D/src/Controlador/CPersona.java
@@ -0,0 +1,33 @@
+package Controlador;
+import Conexion.Conexion;
+import Modelo.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class CPersona {
+    private Conexion conexion = new Conexion();
+    public boolean agregar(Persona persona)
+    {
+        try {
+            Connection cnx = conexion.obtenerConexion();
+            String sql = "INSERT INTO PERSONA (rut, nombre, apellido, direccion) values(?,?,?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(sql);
+            stmt.setString(1, persona.getRut());
+            stmt.setString(2, persona.getNombre());
+            stmt.setString(3, persona.getApellido());
+            stmt.setString(4, persona.getDireccion());
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();            
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CPersona.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+}
